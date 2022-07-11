5266ab82274ead82752dfb01c9accea36a1d8349
casie siekman <casie@soona.co>
2022-07-11 08:52:02 -0500

    move states to client

--- plugins/api-client.js
+++ plugins/api-client.js
@@ -1,3 +1,206 @@
+const states = [
+  {
+    name: 'Alabama',
+    abbr: 'AL'
+  },
+  {
+    name: 'Alaska',
+    abbr: 'AK'
+  },
+  {
+    name: 'Arizona',
+    abbr: 'AZ'
+  },
+  {
+    name: 'Arkansas',
+    abbr: 'AR'
+  },
+  {
+    name: 'California',
+    abbr: 'CA'
+  },
+  {
+    name: 'Colorado',
+    abbr: 'CO'
+  },
+  {
+    name: 'Connecticut',
+    abbr: 'CT'
+  },
+  {
+    name: 'Delaware',
+    abbr: 'DE'
+  },
+  {
+    name: 'Florida',
+    abbr: 'FL'
+  },
+  {
+    name: 'Georgia',
+    abbr: 'GA'
+  },
+  {
+    name: 'Hawaii',
+    abbr: 'HI'
+  },
+  {
+    name: 'Idaho',
+    abbr: 'ID'
+  },
+  {
+    name: 'Illinois',
+    abbr: 'IL'
+  },
+  {
+    name: 'Indiana',
+    abbr: 'IN'
+  },
+  {
+    name: 'Iowa',
+    abbr: 'IA'
+  },
+  {
+    name: 'Kansas',
+    abbr: 'KS'
+  },
+  {
+    name: 'Kentucky',
+    abbr: 'KY'
+  },
+  {
+    name: 'Louisiana',
+    abbr: 'LA'
+  },
+  {
+    name: 'Maine',
+    abbr: 'ME'
+  },
+  {
+    name: 'Maryland',
+    abbr: 'MD'
+  },
+  {
+    name: 'Massachusetts',
+    abbr: 'MA'
+  },
+  {
+    name: 'Michigan',
+    abbr: 'MI'
+  },
+  {
+    name: 'Minnesota',
+    abbr: 'MN'
+  },
+  {
+    name: 'Mississippi',
+    abbr: 'MS'
+  },
+  {
+    name: 'Missouri',
+    abbr: 'MO'
+  },
+  {
+    name: 'Montana',
+    abbr: 'MT'
+  },
+  {
+    name: 'Nebraska',
+    abbr: 'NE'
+  },
+  {
+    name: 'Nevada',
+    abbr: 'NV'
+  },
+  {
+    name: 'New Hampshire',
+    abbr: 'NH'
+  },
+  {
+    name: 'New Jersey',
+    abbr: 'NJ'
+  },
+  {
+    name: 'New Mexico',
+    abbr: 'NM'
+  },
+  {
+    name: 'New York',
+    abbr: 'NY'
+  },
+  {
+    name: 'North Carolina',
+    abbr: 'NC'
+  },
+  {
+    name: 'North Dakota',
+    abbr: 'ND'
+  },
+  {
+    name: 'Ohio',
+    abbr: 'OH'
+  },
+  {
+    name: 'Oklahoma',
+    abbr: 'OK'
+  },
+  {
+    name: 'Oregon',
+    abbr: 'OR'
+  },
+  {
+    name: 'Pennsylvania',
+    abbr: 'PA'
+  },
+  {
+    name: 'Rhode Island',
+    abbr: 'RI'
+  },
+  {
+    name: 'South Carolina',
+    abbr: 'SC'
+  },
+  {
+    name: 'South Dakota',
+    abbr: 'SD'
+  },
+  {
+    name: 'Tennessee',
+    abbr: 'TN'
+  },
+  {
+    name: 'Texas',
+    abbr: 'TX'
+  },
+  {
+    name: 'Utah',
+    abbr: 'UT'
+  },
+  {
+    name: 'Vermont',
+    abbr: 'VT'
+  },
+  {
+    name: 'Virginia',
+    abbr: 'VA'
+  },
+  {
+    name: 'Washington',
+    abbr: 'WA'
+  },
+  {
+    name: 'West Virginia',
+    abbr: 'WV'
+  },
+  {
+    name: 'Wisconsin',
+    abbr: 'WI'
+  },
+  {
+    name: 'Wyoming',
+    abbr: 'WY'
+  },
+]
+
 export default (context, inject) => {
   inject('api', api);
   context.$api = api
@@ -5,12 +208,7 @@ export default (context, inject) => {
 
 const api = {
   async fetchStates() {
-      let response = await fetch('https://reliable-fenglisu-232031.netlify.app/api/states', {
-        method: 'GET',
-        credentials: 'same-origin'
-      });
-      let data = await response.json();
-      return data.states;
+      return states;
   },
 
   async fetchGestationalLimits(state) {
