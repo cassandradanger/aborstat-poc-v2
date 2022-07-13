86838ee58f41aa1ed5f4b7e646e5d699078544f7
casie siekman <casie@soona.co>
2022-07-12 20:58:38 -0500

    add chris

--- pages/index.vue
+++ pages/index.vue
@@ -16,6 +16,7 @@
       <span class="data-attr-wrapper">
         <h6>abortion status - data provided by <a target="_blank" href="http://www.abortionpolicyapi.com">Abortion Policy API</a>, which should be consistent with the data from the <a href=" https://www.guttmacher.org/state-policy/laws-policies">Guttmacher Institute</a></h6>
         <h6>nuxt/expressjs template provided by <a target="_blank" href="https://github.com/nuxt-community/express-template">nuxt-community</a></h6>
+        <h6>logo provided by <a target="_blank" href="https://www.hajny.com">Chris Hajny</a></h6>
       </span>
     </div>
   </div>
